77c55dd27463f56841c29a6d47d7f11ef1386dfd
shahapp <89124032+shahapp@users.noreply.github.com>
2021-08-18 12:28:56 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$conn = mysqli_connect('localhost','id17427043_bank','wbEPbHbJoO99lsr\\', 'id17427043_sparks_bank');
+
+if(!$conn){
+	die("Could not connect to the database due to the following error -->".mysqli_connect_error());
+}
+
+?>
